b943a977747f203e2c95ca3111bad297dcff84c6
Ken <ken.segawa@iths.se>
2023-04-03 19:59:30 +0200

    Added sql script

---
+++ db.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS webshop;
+
+CREATE DATABASE webshop;
+
+USE webshop;
+
+CREATE TABLE product (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	category VARCHAR(255),
+	description VARCHAR(255),
+	name VARCHAR(255),
+	price DOUBLE,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO product (category, description, name, price) VALUES ('Phone', 'The latest iPhone model', 'iPhone 14', 12000.0);
+INSERT INTO product (category, description, name, price) VALUES ('Laptop', 'The fastest MacBook Pro', 'MacBook Pro', 24000.0);
+INSERT INTO product (category, description, name, price) VALUES ('TV', 'New OLED TV from Sony', 'SONY 50" 4K TV', 17000.0);
+INSERT INTO product (category, description, name, price) VALUES ('Phone', 'The latest Samsung phone', 'Samsung Galaxy 22', 11000.0);
+
